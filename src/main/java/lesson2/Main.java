103afb396ee85a56106a9bcee767a6b8d7f227b8
Игорь Родин <igor-rodin@iMac-Igor.local>
2023-02-14 21:57:52 +0300

    Add heapSort

--- src/main/java/lesson2/Main.java
+++ src/main/java/lesson2/Main.java
@@ -3,20 +3,18 @@ package lesson2;
 import java.util.Arrays;
 
 public class Main {
+
+    public static final int ARRAY_SIZE = 20_000;
+
     public static void main(String[] args) {
-        int[] arr = ArrayUtils.prepareArray();
-
+        int[] arr = new int[] {4, 1, 3, 9, 7, -9};
         ArrayUtils.printArray(arr);
-        SortUtils.directSort(arr);
-        ArrayUtils.printArray(arr);
-
-
-        arr = ArrayUtils.prepareArray();
-        ArrayUtils.printArray(arr);
-        SortUtils.quickSort(arr);
+        SortUtils.heapSort(arr);
         ArrayUtils.printArray(arr);
 
-        int[] testArray = ArrayUtils.prepareArray(100000);
+        int[] testArray = ArrayUtils.prepareArray(ARRAY_SIZE);
+
+        System.out.printf("Размер массива: %d\n", ARRAY_SIZE);
         long startTime = System.currentTimeMillis();
         SortUtils.directSort(testArray.clone());
         long endTime = System.currentTimeMillis();
@@ -29,22 +27,28 @@ public class Main {
         processingTime = endTime - startTime;
         System.out.printf("Время работы быстрой сортировки: %d мс.\n", processingTime);
 
-        testArray = new int[]{9, -10, 100, 22, -5, -5, 0, 9, 22, 13};
-        ArrayUtils.printArray(testArray);
-        SortUtils.quickSort(testArray);
-        ArrayUtils.printArray(testArray);
-        int res01 = SearchUtils.binarySearch(testArray, 13);
-        System.out.printf("Элемент %d %s\n",
-                13, res01 >= 0 ? String.format("найден в массиве по индексу %d", res01) :
-                        "не найден в массиве");
+        startTime = System.currentTimeMillis();
+        SortUtils.heapSort(testArray.clone());
+        endTime = System.currentTimeMillis();
+        processingTime = endTime - startTime;
+        System.out.printf("Время работы пирамидальной сортировки: %d мс.\n", processingTime);
 
-        testArray = new int[]{9, -10, 100, 22, -5, -5, 0, 9, 22, 13};
-        Arrays.sort(testArray);
-        ArrayUtils.printArray(testArray);
-        int res02 = Arrays.binarySearch(testArray, 13);
-        System.out.printf("Элемент %d %s\n",
-                13, res02 >= 0 ? String.format("найден в массиве по индексу %d", res02) :
-                        "не найден в массиве");
+//        testArray = new int[]{9, -10, 100, 22, -5, -5, 0, 9, 22, 13};
+//        ArrayUtils.printArray(testArray);
+//        SortUtils.quickSort(testArray);
+//        ArrayUtils.printArray(testArray);
+//        int res01 = SearchUtils.binarySearch(testArray, 13);
+//        System.out.printf("Элемент %d %s\n",
+//                13, res01 >= 0 ? String.format("найден в массиве по индексу %d", res01) :
+//                        "не найден в массиве");
+//
+//        testArray = new int[]{9, -10, 100, 22, -5, -5, 0, 9, 22, 13};
+//        Arrays.sort(testArray);
+//        ArrayUtils.printArray(testArray);
+//        int res02 = Arrays.binarySearch(testArray, 13);
+//        System.out.printf("Элемент %d %s\n",
+//                13, res02 >= 0 ? String.format("найден в массиве по индексу %d", res02) :
+//                        "не найден в массиве");
 
     }
 
--- src/main/java/lesson2/SortUtils.java
+++ src/main/java/lesson2/SortUtils.java
@@ -24,7 +24,7 @@ class SortUtils {
     static void quickSort(int[] array, int startPosition, int endPosition) {
         int left = startPosition;
         int right = endPosition;
-        int middle = array[(left + right) / 2];
+        int middle = array[left + (right - left) / 2];
 
         do {
             while (array[left] < middle) {
@@ -50,6 +50,69 @@ class SortUtils {
         if (startPosition < right) {
             quickSort(array, startPosition, right);
         }
+    }
+
+    /**
+     * Сортирует массив методом пирамидальной сортировки. Исходный массив при этом изменяется
+     * @param array Исходный массив
+     */
+    static void heapSort(int[] array) {
+        buildHeapTree(array);
+        for (int i = array.length - 1; i >= 0 ; i--) {
+            swap(array, 0, i);
+            heapify(array, 0, i);
+        }
+
+    }
+
+    /**
+     * Строит "кучу" из массива array
+     * @param array
+     */
+    private static void buildHeapTree(int[] array) {
+        int size = array.length;
+        for (int i = size / 2 - 1; i >= 0 ; i--) {
+            heapify(array, i, size);
+        }
+    }
+
+    /**
+     * Восстанавливает свойство "кучи" в поддереве array начиная с узла root
+     * @param array Двоичное дерево
+     * @param root Корень поддерева
+     * @param heapSize Размер кучи
+     */
+    private static void heapify(int[] array, int root, int heapSize) {
+        int maxNode = root;
+        int leftNode = maxNode * 2 + 1;
+        int rightNode = maxNode * 2 + 2;
+
+        if (leftNode < heapSize && array[leftNode] > array[maxNode]) {
+            maxNode = leftNode;
+        }
+
+        if (rightNode < heapSize && array[rightNode] > array[maxNode]) {
+            maxNode = rightNode;
+        }
+
+        if (maxNode == root) {
+            return;
+        }
+
+        swap(array, maxNode, root);
+        heapify(array, maxNode, heapSize);
+    }
+
+    /**
+     * Переставляет местами элементы массива array по индексам i, j
+     * @param array
+     * @param i
+     * @param j
+     */
+    private static void swap(int[] array, int i, int j) {
+        int tmp  = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
     }
 
 }
